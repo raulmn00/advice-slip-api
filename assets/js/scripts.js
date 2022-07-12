0dcef2cfeae129a15e2799326f6265e316ba5a28
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-07-12 10:37:35 -0300

    estiliza o botao, adiciona as frases no html e ao carregar a pagina

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,11 @@
 async function getAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
-    console.log(data);
+    document.querySelector("#advice").innerText = data.slip.advice;
 }
 
-getAdvice();
+const getBtnAdvice = document.querySelector("#get-advice");
+
+getBtnAdvice.addEventListener("click", getAdvice);
+
+window.document.onload(getAdvice());
